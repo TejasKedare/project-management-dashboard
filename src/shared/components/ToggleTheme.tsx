ab4f12dec00232bedfe18e2e108b806e130cd707
Tejas Kedare <tejaskedare2@gmail.com>
2026-05-02 13:38:54 +0530

    temporary theme  toggle

--- src/shared/components/ToggleTheme.tsx
+++ src/shared/components/ToggleTheme.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react';
 
 const ToggleTheme = () => {
-  const [currentTheme, setCurrentTheme] = useState('light');
+    const [currentTheme, setCurrentTheme] = useState('light');
+    const handleTheme = (theme: string) => {
+        setCurrentTheme(theme);
+        document.documentElement.classList.toggle('dark', theme === 'dark');
+    };
+    return (
+        <div className="flex items-center bg-gray-100 p-1 rounded-xl shadow-inner w-fit">
 
-  return (
-    <div className="flex items-center bg-gray-100 p-1 rounded-xl shadow-inner w-fit">
-
-      <button
-        onClick={() => setCurrentTheme('light')}
-        className={`px-4 py-1.5 text-sm font-medium rounded-lg transition-all duration-200 cursor-pointer
+            <button
+                onClick={() => handleTheme('light')}
+                className={`px-4 py-1.5 text-sm font-medium rounded-lg transition-all duration-200 cursor-pointer
           ${currentTheme === 'light'
-            ? 'bg-white text-gray-900 shadow'
-            : 'text-gray-500 hover:text-gray-800'}
+                        ? 'bg-white text-gray-900 shadow'
+                        : 'text-gray-500 hover:text-gray-800'}
         `}>
-        Light
-      </button>
+                Light
+            </button>
 
-      <button
-        onClick={() => setCurrentTheme('dark')}
-        className={`px-4 py-1.5 text-sm font-medium rounded-lg transition-all duration-200 cursor-pointer
+            <button
+                onClick={() => handleTheme('dark')}
+                className={`px-4 py-1.5 text-sm font-medium rounded-lg transition-all duration-200 cursor-pointer
           ${currentTheme === 'dark'
-            ? 'bg-white text-gray-900 shadow'
-            : 'text-gray-500 hover:text-gray-800'}
+                        ? 'bg-white text-gray-900 shadow'
+                        : 'text-gray-500 hover:text-gray-800'}
         `}>
-        Dark
-      </button>
+                Dark
+            </button>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default ToggleTheme;
